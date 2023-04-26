1bc2281bcc88bc4f546794717d7ef588b62b4556
Ian Grant <ian@iangrant.me>
2023-04-26 19:00:33 +0100

    Add some more informational output

--- cron-entrypoint.sh
+++ cron-entrypoint.sh
@@ -4,14 +4,17 @@ OP_VAULT="${OP_VAULT:=Automation}"
 USERNAME_FIELDNAME="${USERNAME_FIELDNAME:=username}"
 PASSWORD_FIELDNAME="${PASSWORD_FIELDNAME:=password}"
 EVERY_N_HOURS="${EVERY_N_HOURS:=3}"
+RANDOM_MINUTE=$(( $RANDOM % 59 +0 ))
 
 CMD="withings-sync --garmin-username \$(op read \"op://${OP_VAULT}/${OP_ITEM}/${USERNAME_FIELDNAME}\") --garmin-password \$(op read \"op://${OP_VAULT}/${OP_ITEM}/${PASSWORD_FIELDNAME}\")"
 
 # Run the command once immediately
+echo "Starting initial sync ..."
 eval $CMD
 
 # Then create a cron job to run at a random minute, every N hours
-echo "$(( $RANDOM % 59 +0 )) */${EVERY_N_HOURS} * * * $CMD" > /etc/crontabs/root
+echo "Scheduling sync every ${EVERY_N_HOURS} hours, at ${RANDOM_MINUTE} minutes past"
+echo "$RANDOM_MINUTE */${EVERY_N_HOURS} * * * $CMD" > /etc/crontabs/root
 
 # Execute passthrough args to this script (CMD from Dockerfile unless overriden)
 eval "$@"
