c4815ccb6845f3db74bfe4a2f8939c0def1eec6a
Ian Grant <ian@iangrant.me>
2023-04-26 16:43:49 +0100

    fix: pass GARMIN_ vars to cron job

--- cron-entrypoint.sh
+++ cron-entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 /usr/local/bin/gc_creds_from_1p.sh
-echo "$(( $RANDOM % 59 +0 )) */3 * * * withings-sync -v | tee -a /root/withings-sync.log" > /etc/crontabs/root
+echo "$(( $RANDOM % 59 +0 )) */3 * * * withings-sync --garmin-username $GARMIN_USERNAME --garmin-password $GARMIN_PASSWORD | tee -a /root/withings-sync.log" > /etc/crontabs/root
 crond -f -l 6 -L /dev/stdout
