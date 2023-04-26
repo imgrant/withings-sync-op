88b35e4a13ff65581338f50c473550b8a93e19f5
Ian Grant <ian@iangrant.me>
2023-04-26 17:37:42 +0100

    Switch to fetching the credentials every run

--- cron-entrypoint.sh
+++ cron-entrypoint.sh
@@ -4,5 +4,5 @@ OP_VAULT="${OP_VAULT:=Automation}"
 USERNAME_FIELDNAME="${USERNAME_FIELDNAME:=username}"
 PASSWORD_FIELDNAME="${PASSWORD_FIELDNAME:=password}"
 EVERY_N_HOURS="${EVERY_N_HOURS:=3}"
-echo "$(( $RANDOM % 59 +0 )) */${EVERY_N_HOURS} * * * withings-sync --garmin-username $(op read "op://${OP_VAULT}/${OP_ITEM}/${USERNAME_FIELDNAME}") --garmin-password $(op read "op://${OP_VAULT}/${OP_ITEM}/${PASSWORD_FIELDNAME}") | tee -a /root/withings-sync.log" > /etc/crontabs/root
+echo "$(( $RANDOM % 59 +0 )) */${EVERY_N_HOURS} * * * withings-sync --garmin-username \$(op read \"op://${OP_VAULT}/${OP_ITEM}/${USERNAME_FIELDNAME}\") --garmin-password \$(op read \"op://${OP_VAULT}/${OP_ITEM}/${PASSWORD_FIELDNAME}\") | tee -a /root/withings-sync.log" > /etc/crontabs/root
 crond -f -l 6 -L /dev/stdout
