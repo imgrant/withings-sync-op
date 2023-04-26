dff07533e0eb466c9ea068968703925d651153c6
Ian Grant <ian@iangrant.me>
2023-04-26 18:51:03 +0100

    Run the sync at first startup, _then_ start the cron schedule

--- cron-entrypoint.sh
+++ cron-entrypoint.sh
@@ -4,5 +4,14 @@ OP_VAULT="${OP_VAULT:=Automation}"
 USERNAME_FIELDNAME="${USERNAME_FIELDNAME:=username}"
 PASSWORD_FIELDNAME="${PASSWORD_FIELDNAME:=password}"
 EVERY_N_HOURS="${EVERY_N_HOURS:=3}"
-echo "$(( $RANDOM % 59 +0 )) */${EVERY_N_HOURS} * * * withings-sync --garmin-username \$(op read \"op://${OP_VAULT}/${OP_ITEM}/${USERNAME_FIELDNAME}\") --garmin-password \$(op read \"op://${OP_VAULT}/${OP_ITEM}/${PASSWORD_FIELDNAME}\") | tee -a /root/withings-sync.log" > /etc/crontabs/root
+
+CMD="withings-sync --garmin-username \$(op read \"op://${OP_VAULT}/${OP_ITEM}/${USERNAME_FIELDNAME}\") --garmin-password \$(op read \"op://${OP_VAULT}/${OP_ITEM}/${PASSWORD_FIELDNAME}\")"
+
+# Run the command once immediately
+eval $CMD
+
+# Then create a cron job to run at a random minute, every N hours
+echo "$(( $RANDOM % 59 +0 )) */${EVERY_N_HOURS} * * * $CMD" > /etc/crontabs/root
+
+# Execute passthrough args to this script (CMD from Dockerfile unless overriden)
 eval "$@"
